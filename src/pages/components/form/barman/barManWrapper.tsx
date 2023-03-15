74f07820088be8c1c3e5d0987b01977352a8ebd5
hossemedineali <hossemedine@outlook.com>
2023-03-15 02:07:46 +0100

    added scanner

--- src/pages/components/form/barman/barManWrapper.tsx
+++ src/pages/components/form/barman/barManWrapper.tsx
@@ -1,9 +1,21 @@
-
+import { useState } from 'react';
+import QrCodeReader, { QRCode } from 'react-qrcode-reader';
 
 const BarManWrapper = () => {
-    return ( <div>
 
-    </div> );
-}
- 
+    const [val, setVal] = useState<string>('');
+    const handleRead = (code: QRCode) => {
+        setVal(code.data);
+      };
+    
+  return (
+    <div className=" h-screen pt-4">
+      <div className="h-64 max-h-[80vh] w-full border">
+      <QrCodeReader delay={100} width={500} height={500} onRead={handleRead} />
+
+      </div>
+    </div>
+  );
+};
+
 export default BarManWrapper;
